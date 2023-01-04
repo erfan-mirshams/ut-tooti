b97a2d597cc3b843a8566f45327e7353d2d80d98
Erfan Mirshams <erfanmirshams@gmail.com>
2023-01-04 22:21:13 +0330

    remove struct.c and add structs to header

--- struct.h
+++ struct.h
@@ -3,4 +3,29 @@
 typedef struct user user;
 typedef struct post post;
 typedef struct like like;
+
+struct user {
+    int id;
+    char *name;
+    char *pass;
+    int post_cnt;
+    user *next;
+    user *prev;
+};
+
+struct post {
+    int id;
+    int user_id;
+    int like_cnt;
+    char *content;
+    post *next;
+    post *prev;
+};
+
+struct like {
+    int user_id;
+    int post_id;
+    like *next;
+    like *prev;
+};
 #endif //STRUCT_H_
